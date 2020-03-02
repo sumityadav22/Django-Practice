76473e6cadd34023039c4cd5ad36880c19751bf5
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-02 23:05:19 +0530

    EDIT - Retrieve any column of the model

--- learnDjango/SportApp/models.py
+++ learnDjango/SportApp/models.py
@@ -6,3 +6,7 @@ class News(models.Model):
     auther = models.CharField(max_length = 30)
     title = models.CharField(max_length = 20)
     description = models.CharField(max_length = 40)
+
+#This is how we can 
+def __str__(self):
+    return self.title
