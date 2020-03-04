79b53b1d780730b44690acfcbca1d803a6250156
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-04 22:42:01 +0530

    EDIT - {ADD} linking of urls

--- learnDjango/SportApp/urls.py
+++ learnDjango/SportApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import News, Home,  Contact
 
 urlpatterns = [
+    # Note : Using this name we can do the 'url linking'
     path('', Home, name = 'Home'),
     path('news/', News, name = 'News'),
     path('contact/', Contact, name = 'Contact'),
