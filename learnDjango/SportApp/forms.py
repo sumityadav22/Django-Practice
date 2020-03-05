ac1f58866c46a8dfde0f598257deeabdb9136248
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-06 02:28:04 +0530

    EDIT -  { ADD } Convert HTML form to Django form

--- learnDjango/SportApp/forms.py
+++ learnDjango/SportApp/forms.py
@@ -3,7 +3,23 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length = 100)
-    password = forms.CharField(max_length = 100)
-    email = forms.CharField(max_length = 100)
-    phone = forms.CharField(max_length = 100)
+    username = forms.CharField(max_length = 100,
+                            widget = forms.TextInput(attrs = {
+                                    'class':'form-control',
+                                    'placeholder':'Enter your username'
+                                        }))
+    password = forms.CharField(max_length = 100,
+                            widget = forms.PasswordInput(attrs = {
+                                    'class':'form-control',
+                                    'placeholder':'Enter your password'
+                                        }))
+    email = forms.CharField(max_length = 100,
+                                widget = forms.EmailInput(attrs = {
+                                    'class':'form-control',
+                                    'placeholder':'Enter your email'
+                                        }))
+    phone = forms.CharField(max_length = 100,
+                                    widget = forms.NumberInput(attrs = {
+                                    'class':'form-control',
+                                    'placeholder':'Enter your phone number'
+                                        }))
