e70333870e85a5ae51d52cb44473737109b387af
sumityadav22 <sumityadavs5574@gmail.com>
2020-03-06 02:44:54 +0530

    EDIT - Navbar style

--- learnDjango/SportApp/models.py
+++ learnDjango/SportApp/models.py
@@ -14,7 +14,7 @@ def __str__(self):
 
 
 class RegistrationData(models.Model):
-    username = models.CharField(max_length = 100),
+    username = models.CharField(max_length = 100)
     password = models.CharField(max_length = 100)
     email = models.CharField(max_length = 100)
     phone = models.CharField(max_length = 100)
